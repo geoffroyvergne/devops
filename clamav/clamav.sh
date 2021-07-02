596b59d98c3671ae9e25c54d6e020ffb7ecafd31
Geofroy Vergne <geoffroy.vergne@gmail.com>
2021-07-02 20:40:42 +0200

    updates

--- clamav/clamav.sh
+++ clamav/clamav.sh
@@ -7,4 +7,3 @@ rm ~/clamscan_report_**.txt
 clamscan -r / --exclude-dir="/Volumes" --exclude-dir="/Users" -i -l ~/clamscan_report_root.txt
 clamscan -r ~/ --exclude-dir="~/data" --exclude-dir="onedrive" --exclude-dir="Dropbox" --exclude-dir="Google\ Drive\ Perso" --exclude-dir="VirtualBox\ VM" --exclude-dir="VirtualMachines" -i -l ~/clamscan_report_home.txt
 clamscan -r ~/data -i -l ~/clamscan_report_data.txt
-
